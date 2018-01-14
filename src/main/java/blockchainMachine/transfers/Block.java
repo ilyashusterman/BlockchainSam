5eb5f0371593ec3f77d980534978bcd376ddc1b5
ilya shusterman <ilyashusterman@outlook.com>
2018-01-14 03:51:04 +0200

    initial commit

---
+++ src/main/java/blockchainMachine/transfers/Block.java
@@ -0,0 +1,4 @@
+package blockchainMachine.transfers;
+
+public class Block {
+}
---
+++ src/test/java/blockchainMachine/BlockTest.java
@@ -0,0 +1,20 @@
+package blockchainMachine;
+
+import blockchainMachine.transfers.Block;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockTest {
+
+    private Block block;
+
+    @Before
+    public void setUp() throws Exception {
+        block = new Block();
+    }
+
+    @Test
+    public void createBlockAfterBlock() throws Exception {
+
+    }
+}
